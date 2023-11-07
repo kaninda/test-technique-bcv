8560bb14331312fe240e722847262c724d48927b
kaninda <73389238+kaninda@users.noreply.github.com>
2023-11-07 13:45:15 +0100

    set up test

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    private Integer salary ;
+    private String department;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1. Creer un liste des eemployees avec les parametres suivants
+        /*      new Employee("Arnaud", 2000, "IT")
+                new Employee("Kaninda", 3200, "IT")
+                new Employee("Aktar", 2300, "IT")
+                new Employee("Nabil", 4200, "RH")
+                new Employee("kmihi", 2900, "RH")
+                new Employee("Axel", 3200, "RH") */
+        // 2. Ecrire un progrqmme qui trouve le plus grand salaire par departement
     }
 }
